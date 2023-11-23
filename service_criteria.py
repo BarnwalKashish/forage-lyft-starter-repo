4fe4780e11809c34ef157dcb870ca7e8e74ed375
BarnwalKashish <151847088+BarnwalKashish@users.noreply.github.com>
2023-11-24 02:08:32 +0530

    Create service_criteria.py

---
+++ service_criteria.py
@@ -0,0 +1,7 @@
+# service_criteria.py
+class ServiceCriteria:
+    def __init__(self, service_interval):
+        self.service_interval = service_interval
+
+    def get_service_interval(self):
+        return self.service_interval
